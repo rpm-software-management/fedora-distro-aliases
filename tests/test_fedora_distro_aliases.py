81f042d5bdd0ef89d6c1c3ef51aeb94127402e66
Jakub Kadlcik <frostyx@email.cz>
2024-03-23 23:35:52 +0100

    Move mock_responses to a file shared with all tests

--- tests/test_fedora_distro_aliases.py
+++ tests/test_fedora_distro_aliases.py
@@ -1,27 +1,7 @@
-import os
-import json
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from munch import Munch
 from fedora_distro_aliases import Distro, get_distro_aliases
-
-here = os.path.abspath(os.path.dirname(__file__))
-datadir = os.path.join(here, "data")
-
-
-def mock_responses(files):
-    """
-    Every `bodhi_active_releases` call sends 3 HTTP requests. To comfortably
-    mock their responses from saved files, we can use this function.
-    """
-    responses = []
-    for name in files:
-        path = os.path.join(datadir, name)
-        with open(path, "r") as fp:
-            data = json.load(fp)
-        response = MagicMock()
-        response.json.return_value = data
-        responses.append(response)
-    return responses
+from . import mock_responses
 
 
 def test_distro():
