8ef62dbf7a1311f9d09afbe84bde887a1147c23b
Jakub Kadlcik <frostyx@email.cz>
2024-10-07 22:48:46 +0200

    Ignore packages in the tests directory
    Otherwise our package build fails with:
    
        RPM build errors:
        error: Installed (but unpackaged) file(s) found:

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     classifiers=[],
     setup_requires=setup_requires,
     install_requires=install_requires,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
     py_modules=["cli"],
